e5bafe1ca070763baf427b9af36bccd034f81327
ancoulon <ancoulon@student.s19.be>
2020-10-05 12:22:16 +0200

    updated env-42

--- .zsh/env-42.zsh
+++ .zsh/env-42.zsh
@@ -1,5 +1,11 @@
 # homebrew path
-export PATH="$PATH:~/usr/bin"
+export PATH="/Users/ancoulon/usr/bin:$PATH"
 
 # homebrew cask
 export HOMEBREW_CASK_OPTS="--appdir=~/Applications --caskroom=~/usr/Caskroom"
+
+# vscode command line utility
+export PATH="/Applications/Visual Studio Code 2.app/Contents/Resources/app/bin:$PATH"
+
+# 42header config
+export MAIL="ancoulon@student.s19.be"
